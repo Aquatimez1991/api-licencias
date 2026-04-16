5ea264fa9aaed47e21a87fe47c24283545c8b552
Aquatimez1991 <esalgadoc@outlook.com>
2026-04-15 21:10:43 -0500

    Initial commit

---
+++ api-licencias/src/main/java/com/sistema/licencias/ApiLicenciasApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.licencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLicenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLicenciasApplication.class, args);
+	}
+
+}
